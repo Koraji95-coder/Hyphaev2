e29c202b3f13c7c3fe39f40f6847c11ee238c419
Dustin <dustinwardelectricalengineer@outlook.com>
2025-07-04 16:12:53 -0500

    Switch weather service to OpenWeather

--- frontend/vite-env.d.ts
+++ frontend/vite-env.d.ts
@@ -2,7 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_OPENCAGE_API_KEY: string;
-  readonly VITE_WEATHER_API_KEY: string;
+  readonly VITE_OPENWEATHER_API_KEY: string;
   // add any other VITE_ vars here…
 }
 
